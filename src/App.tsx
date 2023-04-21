2c002230cf0d5f9169aaa108cf0c9f2ba5ea8a2c
Joel Rosas <joeli.rosas20@gmail.com>
2023-04-21 18:24:24 -0400

    Started adding the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Home from './components/pages/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <Router>
+        <Navbar/>
         <Routes>
           <Route path='/' Component={Home}/>
         </Routes>
