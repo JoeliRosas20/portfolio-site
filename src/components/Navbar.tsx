9a5311684689dfbf652c677f47c934bc455d692c
Joel Rosas <joeli.rosas20@gmail.com>
2023-04-21 18:56:41 -0400

    Added more content in the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)//click is switched to true
+    const closeMobileMenu = () => setClick(false)
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                Joel's Resume Site 
-            </Link>
+          <Link to="/" className="navbar-title">
+            Joel's Resume Site
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+          </div>
+          <ul>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                About Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Contact Info
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
