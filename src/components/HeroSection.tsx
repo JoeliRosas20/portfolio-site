39ea75c1a3a7f9604610206846760332ee5474d8
Joel Rosas <joeli.rosas20@gmail.com>
2023-04-25 11:59:54 -0400

    Just added the herosection to the page

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div>
+        <h1>Welcome to my resume</h1>
+        <p>My name is Joel Rosas</p>
+    </div>
+  )
+}
+
+export default HeroSection
