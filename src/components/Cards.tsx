f31495a7f4226038f007f94b7286d07ea8cd752c
Joel Rosas <joeli.rosas20@gmail.com>
2023-04-26 13:49:43 -0400

    Added cards comp and started to edit hero section

---
+++ src/components/Cards.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Cards() {
+  return <div className="cards">
+    <h1>These are my projects</h1>
+  </div>;
+}
+
+export default Cards;
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div>
+    <div className='hero-container'>
         <h1>Welcome to my resume</h1>
         <p>My name is Joel Rosas</p>
     </div>
