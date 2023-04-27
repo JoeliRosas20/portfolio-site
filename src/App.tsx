ff74676c98040d4cd3c19ba73cccd87a03400252
Joel Rosas <joeli.rosas20@gmail.com>
2023-04-27 11:05:05 -0400

    Added the other pages and worked on css for Hero

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Home from './components/pages/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import AboutMe from './components/pages/AboutMe'
+import ContactInfo from './components/pages/ContactInfo'
+import Projects from './components/pages/Projects'
 
 function App() {
 
@@ -11,6 +14,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' Component={Home}/>
+          <Route path='/aboutme' Component={AboutMe}/>
+          <Route path='/projects' Component={Projects}/>
+          <Route path='/contactinfo' Component={ContactInfo}/>
         </Routes>
       </Router>
     </>
