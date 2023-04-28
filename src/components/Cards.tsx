507485c3a9dbbd04ba342b05716659e903ba1f06
Joel Rosas <joeli.rosas20@gmail.com>
2023-04-27 21:37:38 -0400

    Finished working on the cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import CardItem from "./CardItem";
-import "./Cards.css"
+import "./Cards.css";
 
 function Cards() {
-  return <div className="cards">
-    <h1>These are my projects</h1>
-    <div className="card_container">
-      <div className="card_wrapper">
-        <div className="card_items">
-          <CardItem
-            src="images/ExerciseSite.png"
-            text="My exercise site"
-            label="Exercise Site"
-            path="/projects"
-          />
+  return (
+    <div className="cards">
+      <h1>These are my projects</h1>
+      <div className="cards_container">
+        <div className="cards_wrapper">
+          <div className="cards_items">
+            <CardItem
+              src="images/ExerciseSite.png"
+              text="Useful with tracking my exercises"
+              label="Exercise Site"
+              path="/projects"
+            />
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default Cards;
