901ea3cf3d411cd8d91e9b23c8196526c1e938b9
Joel Rosas <joeli.rosas20@gmail.com>
2023-04-28 14:12:46 -0400

    Started working on the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <h1>Socials</h1>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
