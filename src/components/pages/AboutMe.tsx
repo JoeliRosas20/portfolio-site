f3c8d866b846d548cbe724a88df03e9d83d3a3bf
Joel Rosas <joeli.rosas20@gmail.com>
2023-05-01 21:10:45 -0400

    Added more content to the About Me page

--- src/components/pages/AboutMe.tsx
+++ src/components/pages/AboutMe.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import './AboutMe.css'
+import React from "react";
+import "./AboutMe.css";
 
 function AboutMe() {
   return (
-    <div className='aboutme-container'>
-      <h1>Who am I?</h1>
-      <h2>Education</h2>
-      <h2>Skills</h2>
-      <p>I am a software developer who has a passion for learning and wanting to help others.</p>
-      <br/>
-      <p>I began to code when I was in college.</p>
-      <br/>
-      <p>From there, it all went well.</p>
+    <div className="aboutme-container">
+      <div className="aboutme-education">
+        <h1>Who am I?</h1>
+        <h2>Education</h2>
+        <img src="/images/college.png" />
+      </div>
+      <div className="aboutme-skills">
+        <h2>Skills</h2>
+        <i className="fa-brands fa-html5"></i>
+        <i className="fa-brands fa-css3-alt"></i>
+        <i className="fa-brands fa-js"></i>
+        <i className="fa-brands fa-java"></i>
+      </div>
+      <div className="aboutme-summary">
+        <p>
+          I am a software developer who has a passion for learning and wanting
+          to help others.
+        </p>
+        <br />
+        <p>I began to code when I was in college.</p>
+        <br />
+        <p>From there, it all went well.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
