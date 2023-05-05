316c56a5c3c25bb648c4f945626b874b8975266e
Joel Rosas <joeli.rosas20@gmail.com>
2023-05-04 21:48:07 -0400

    Started working on ContactInfo and edit to Project

--- src/components/pages/ContactInfo.tsx
+++ src/components/pages/ContactInfo.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './ContactInfo.css'
 
 function ContactInfo() {
   return (
-    <div>
+    <div className='contactInfo-container'>
       <h1>Let's connect!!!</h1>
+      <form>
+        <label htmlFor='name'>Name</label>
+        <input
+          type='text'
+          id='name'
+        />
+        <br/>
+        <label htmlFor='email'>Email</label>
+        <input
+          type='text'
+          id='email'
+        />
+        <br/>
+        <label htmlFor='message' className='messageLabel'>Message</label>
+        <textarea
+          rows="10"
+          cols="30"
+          id="message"
+        ></textarea>
+        <br/>
+        <button type='submit'>submit</button>
+      </form>
     </div>
   )
 }
